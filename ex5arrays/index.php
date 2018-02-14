e668eaa9fa27ed038ddbeaa1dd809d28d6cc3276
mariomliberio <mariomliberio@gmail.com>
2018-02-14 11:00:53 +0100

    parametres fini

---
+++ ex5arrays/index.php
@@ -0,0 +1,42 @@
+<?php
+const BR = "<br />";
+$mois = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
+print_r($mois);
+echo BR;
+echo $mois[2];
+echo BR;
+echo $mois [5];
+echo BR;
+$mois[7] = "Août";
+echo $mois[7];
+echo BR;
+$departements[1] = "Ain";
+$departements[3] = "Allier";
+$departements[7] = "Ardeche";
+$departements[15] = "Cantal";
+$departements[26] = "Drome";
+$departements[38] = "Isere";
+$departements[42] = "Loire";
+$departements[43] = "Haute-Loire";
+echo $departements[57] = "Moselle";
+$departements[63] = "Puy-de-Dome";
+$departements[69] = "Rhone";
+$departements[73] = "Savoie";
+$departements[74] = "Haute-Savoie";
+echo BR;
+echo $departements[69];
+echo BR;
+print_r($departements);
+echo BR;
+
+
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,35 +1,7 @@
 <?php
-const BR = "<br />";
-$mois = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
-print_r($mois);
-echo BR;
-echo $mois[2];
-echo BR;
-echo $mois [5];
-echo BR;
-$mois[7] = "Août";
-echo $mois[7];
-echo BR;
-$departements[1] = "Ain";
-$departements[3] = "Allier";
-$departements[7] = "Ardeche";
-$departements[15] = "Cantal";
-$departements[26] = "Drome";
-$departements[38] = "Isere";
-$departements[42] = "Loire";
-$departements[43] = "Haute-Loire";
-echo $departements[57] = "Moselle";
-$departements[63] = "Puy-de-Dome";
-$departements[69] = "Rhone";
-$departements[73] = "Savoie";
-$departements[74] = "Haute-Savoie";
-echo BR;
-echo $departements[69];
-echo BR;
-print_r($departements);
-echo BR;
 
- ?>
+
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -37,5 +9,6 @@ echo BR;
     <title></title>
   </head>
   <body>
+
   </body>
 </html>
